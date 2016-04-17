40cbb83b5d1ff5f6d8821512ccda83d136bd2e88
Kamille Delgardo <Kamilleski@users.noreply.github.com>
2016-04-17 19:34:02 -0400

    School factory, seed data, pagination (#2)
    * School factory, seed data, pagination
    
    * Appease the hound
    
    * Appease the hound

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,9 +1,9 @@
 class SchoolsController < ApplicationController
   def index
     if params[:search]
-      @schools = School.search(params[:search]).order(:name)
+      @schools = School.search(params[:search]).order(:name).page params[:page]
     else
-      @schools = School.all.order(:name)
+      @schools = School.all.order(:name).page params[:page]
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -53,3 +53,423 @@ School.create(
   logo: "https://upload.wikimedia.org/wikipedia/en/b/b7/Stanford_University_seal_2003.svg",
   mascot: "Cardinal",
   date_founded: 1891)
+
+School.create(
+  name: "Massachusetts Institute of Technology",
+  nickname: "MIT",
+  city: "Cambridge",
+  state: "MA",
+  motto: "Mens et Manus",
+  email_domain: "mit.edu",
+  undergrad_population: 4512,
+  graduate_population: 6807,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/4/44/MIT_Seal.svg",
+  mascot: "Tim the Beaver",
+  date_founded: 1860)
+
+School.create(
+  name: "University of Michigan - Ann Arbor",
+  nickname: "U of M",
+  city: "Ann Arbor",
+  state: "MI",
+  motto: "Artes, Scientia, Veritas",
+  email_domain: "umich.edu",
+  undergrad_population: 28395,
+  graduate_population: 15230,
+  public: true,
+  logo: "https://upload.wikimedia.org/wikipedia/en/b/b8/Umichigan_color_seal.png",
+  mascot: "Wolverine",
+  date_founded: 1817)
+
+School.create(
+  name: "California Institute of Technology",
+  nickname: "Caltech",
+  city: "Pasadena",
+  state: "CA",
+  motto: "The truth shall make you free",
+  email_domain: "caltech.edu",
+  undergrad_population: 1001,
+  graduate_population: 1254,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/e/ef/Caltech_logo.svg",
+  mascot: "Beaver",
+  date_founded: 1891)
+
+School.create(
+  name: "University of Chicago",
+  nickname: "U of C",
+  city: "Chicago",
+  state: "IL",
+  motto: "Crescat scientia; vita excolatur",
+  email_domain: "uchicago.edu",
+  undergrad_population: 5724,
+  graduate_population: 9588,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/0/0d/UChicago_presidential_seal.svg",
+  mascot: "Phoenix",
+  date_founded: 1890)
+
+School.create(
+  name: "Princeton University",
+  nickname: "Princeton",
+  city: "Princeton",
+  state: "NJ",
+  motto: "Dei Sub Numine Viget",
+  email_domain: "princeton.edu",
+  undergrad_population: 5391,
+  graduate_population: 2697,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/7/71/Princeton_shield.svg",
+  mascot: "Tiger",
+  date_founded: 1746)
+
+School.create(
+  name: "Yale University",
+  nickname: "Yale",
+  city: "New Haven",
+  state: "CT",
+  motto: "אורים ותמים",
+  email_domain: "princeton.edu",
+  undergrad_population: 5453,
+  graduate_population: 6859,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/commons/0/07/Yale_University_Shield_1.svg",
+  mascot: "",
+  date_founded: 1701)
+
+School.create(
+  name: "Johns Hopkins University",
+  nickname: "Johns Hopkins",
+  city: "Baltimore",
+  state: "MD",
+  motto: "Veritas vos liberabit",
+  email_domain: "jhu.edu",
+  undergrad_population: 6023,
+  graduate_population: 14848,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/6/6d/Johns_Hopkins_University%27s_Academic_Seal.png",
+  mascot: "Blue Jay",
+  date_founded: 1876)
+
+School.create(
+  name: "Cornell University",
+  nickname: "Cornell",
+  city: "Ithaca",
+  state: "NY",
+  motto: "'I would found an institution where any person can find instruction in any study.'",
+  email_domain: "cornell.edu",
+  undergrad_population: 14315,
+  graduate_population: 7589,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/b/bc/Cornell_Seal.svg",
+  mascot: "Touchdown the Bear",
+  date_founded: 1865)
+
+School.create(
+  name: "University of Pennsylvania",
+  nickname: "UPenn",
+  city: "Philadelphia",
+  state: "PA",
+  motto: "Leges sine moribus vanane",
+  email_domain: "upenn.edu",
+  undergrad_population: 10406,
+  graduate_population: 11035,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/commons/9/92/UPenn_shield_with_banner.svg",
+  mascot: "Quaker",
+  date_founded: 1740)
+
+School.create(
+  name: "Columbia University in the City of New York",
+  nickname: "Columbia",
+  city: "New York City",
+  state: "New York",
+  motto: "In lumino Tuo videbimus lumen",
+  email_domain: "columbia.edu",
+  undergrad_population: 8410,
+  graduate_population: 19532,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/e/e8/ColumbiaNYUCoat.svg",
+  mascot: "Columbia Lions",
+  date_founded: 1754)
+
+School.create(
+  name: "University of California, Los Angeles",
+  nickname: "UCLA",
+  city: "Los Angeles",
+  state: "CA",
+  motto: "Fiat lux",
+  email_domain: "ucla.edu",
+  undergrad_population: 29585,
+  graduate_population: 12323,
+  public: true,
+  logo: "http://www.pies.ucla.edu/img/UCLA_seal.gif",
+  mascot: "Bruins",
+  date_founded: 1919)
+
+School.create(
+  name: "Northwestern University",
+  nickname: "Northwestern",
+  city: "Chicago",
+  state: "IL",
+  motto: "Ὁ Λόγος πλήρης χάριτος καὶ ἀληθείας",
+  email_domain: "northwestern.edu",
+  undergrad_population: 9001,
+  graduate_population: 12641,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/6/69/Northwestern_University_Seal.svg",
+  mascot: "Willie the Wildcat",
+  date_founded: 1851)
+
+School.create(
+  name: "University of California, San Diego",
+  nickname: "UC San Diego",
+  city: "San Diego",
+  state: "CA",
+  motto: "Fiat lux",
+  email_domain: "ucsd.edu",
+  undergrad_population: 26590,
+  graduate_population: 7145,
+  public: true,
+  logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uc_seal_black.png/200px-Uc_seal_black.png",
+  mascot: "King Triton",
+  date_founded: 1960)
+
+School.create(
+  name: "Brown University",
+  nickname: "Brown",
+  city: "Providence",
+  state: "RI",
+  motto: "In Deo Speramus",
+  email_domain: "brown.edu",
+  undergrad_population: 6320,
+  graduate_population: 2230,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/0/07/BrownU-CoA.svg",
+  mascot: "Bruno the Bear",
+  date_founded: 1764)
+
+School.create(
+  name: "New York University",
+  nickname: "NYU",
+  city: "New York City",
+  state: "NY",
+  motto: "Perstare et praestare",
+  email_domain: "nyu.edu",
+  undergrad_population: 24985,
+  graduate_population: 24289,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/1/16/New_York_University_Seal.svg",
+  mascot: "Bobcat",
+  date_founded: 1831)
+
+School.create(
+  name: "University of Wisconsin–Madison",
+  nickname: "UW",
+  city: "Madison",
+  state: "WI",
+  motto: "Numen Lumen",
+  email_domain: "wisc.edu",
+  undergrad_population: 29302,
+  graduate_population: 11904,
+  public: true,
+  logo: "https://upload.wikimedia.org/wikipedia/commons/e/e4/NumenLumen.svg",
+  mascot: "Bucky Badger",
+  date_founded: 1848)
+
+School.create(
+  name: "University of Illinois at Urbana–Champaign",
+  nickname: "Illinois",
+  city: "Champaign-Urbana",
+  state: "IL",
+  motto: "Learning and Labor",
+  email_domain: "illinois.edu",
+  undergrad_population: 32878,
+  graduate_population: 11209,
+  public: true,
+  logo: "https://s.graphiq.com/sites/default/files/10/media/images/University_of_Illinois_at_Urbana-Champaign_220154.png",
+  mascot: "Fighting Illini",
+  date_founded: 1867)
+
+School.create(
+  name: "Carnegie Mellon University",
+  nickname: "Carnegie Mellon",
+  city: "Pittsburgh",
+  state: "PA",
+  motto: "'My heart is in the work'",
+  email_domain: "cmu.edu",
+  undergrad_population: 6362,
+  graduate_population: 7141,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/b/bb/Carnegie_Mellon_University_seal.svg",
+  mascot: "Scotty the Scottish Terrier",
+  date_founded: 1900)
+
+School.create(
+  name: "Williams College",
+  nickname: "Williams",
+  city: "Williamstown, Massachusetts",
+  state: "MA",
+  motto: "E liberalitate E. Williams, armigeri",
+  email_domain: "williams.edu",
+  undergrad_population: 2052,
+  graduate_population: 54,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/1/19/Williams_College_Seal.svg",
+  mascot: "The Purple Cow",
+  date_founded: 1793)
+
+School.create(
+  name: "Amherst College",
+  nickname: "Amherst",
+  city: "Amherst",
+  state: "MA",
+  motto: "Terras Irradient",
+  email_domain: "amherst.edu",
+  undergrad_population: 1795,
+  graduate_population: 0,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/6/65/Amherst_College_Seal.svg",
+  mascot: "Lord Jeffrey Amherst (unofficial)",
+  date_founded: 1821)
+
+School.create(
+  name: "Swarthmore College",
+  nickname: "Swarthmore",
+  city: "Swarthmore",
+  state: "PA",
+  motto: "Mind the Light",
+  email_domain: "swarthmore.edu",
+  undergrad_population: 1581,
+  graduate_population: 0,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/8/8e/Formal_Seal_of_Swarthmore_College%2C_Swarthmore%2C_PA%2C_USA.svg",
+  mascot: "Phineas the Phoenix",
+  date_founded: 1864)
+
+School.create(
+  name: "Bowdoin College",
+  nickname: "Bowdoin",
+  city: "Brunswick",
+  state: "ME",
+  motto: "Ut Aquila Versus Coelum",
+  email_domain: "bowdoin.edu",
+  undergrad_population: 1799,
+  graduate_population: 0,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/e/e8/Formal_Seal_of_Bowdoin_College%2C_Brunswick%2C_ME%2C_USA.svg",
+  mascot: "Polar Bear",
+  date_founded: 1794)
+
+School.create(
+  name: "Middlebury College",
+  nickname: "Middlebury",
+  city: "Middlebury",
+  state: "VT",
+  motto: "Scientia et Virtus",
+  email_domain: "",
+  undergrad_population: 2526,
+  graduate_population: 0,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/0/0f/Middlebury_seal.png",
+  mascot: "Panther",
+  date_founded: 1800)
+
+School.create(
+  name: "Pomona College",
+  nickname: "Pomona",
+  city: "Claremont",
+  state: "CA",
+  motto: "Let only the eager, thoughtful, and reverent enter here.",
+  email_domain: "pomona.edu",
+  undergrad_population: 1663,
+  graduate_population: 0,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/9/9d/Formal_Seal_of_Pomona_College%2C_Claremont%2C_CA%2C_USA.svg",
+  mascot: "Sagehens",
+  date_founded: 1887)
+
+School.create(
+  name: "Wellesley College",
+  nickname: "Wellesley",
+  city: "Wellesley",
+  state: "MA",
+  motto: "Non Ministrari sed Ministrare",
+  email_domain: "wellesley.edu",
+  undergrad_population: 2474,
+  graduate_population: 0,
+  public: false,
+  logo: "https://upload.wikimedia.org/wikipedia/en/6/6b/Formal_Seal_of_Wellesley_College%2C_Wellesley%2C_MA%2C_USA.svg",
+  mascot: "Blue",
+  date_founded: 1875)
+
+School.create(
+  name: "University of Texas at Austin",
+  nickname: "UT Austin",
+  city: "Austin",
+  state: "TX",
+  motto: "Disciplina praesidium civitatis",
+  email_domain: "utexas.edu",
+  undergrad_population: 39523,
+  graduate_population: 11790,
+  public: true,
+  logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Large_university-of-texas_seal_rgb%28199-91-18%29.png",
+  mascot: "Longhorn",
+  date_founded: 1883)
+
+School.create(
+  name: "University of Maryland, College Park",
+  nickname: "UMCP",
+  city: "College Park",
+  state: "MD",
+  motto: "Fatti maschii, parole femine",
+  email_domain: "umd.edu",
+  undergrad_population: 27443,
+  graduate_population: 10697,
+  public: true,
+  logo: "https://upload.wikimedia.org/wikipedia/en/5/51/University_of_Maryland_Seal.svg",
+  mascot: "Testudo",
+  date_founded: 1856)
+
+# School.create(
+#   name: "",
+#   nickname: "",
+#   city: "",
+#   state: "",
+#   motto: "",
+#   email_domain: "",
+#   undergrad_population: XX,
+#   graduate_population: XX,
+#   public: bool,
+#   logo: "",
+#   mascot: "",
+#   date_founded: XX)
+
+# School.create(
+#   name: "",
+#   nickname: "",
+#   city: "",
+#   state: "",
+#   motto: "",
+#   email_domain: "",
+#   undergrad_population: XX,
+#   graduate_population: XX,
+#   public: bool,
+#   logo: "",
+#   mascot: "",
+#   date_founded: XX)
+
+# School.create(
+#   name: "",
+#   nickname: "",
+#   city: "",
+#   state: "",
+#   motto: "",
+#   email_domain: "",
+#   undergrad_population: XX,
+#   graduate_population: XX,
+#   public: bool,
+#   logo: "",
+#   mascot: "",
+#   date_founded: XX)
