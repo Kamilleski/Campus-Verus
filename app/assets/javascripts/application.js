d9308d54de290c227ce50a3dae9c45fd54143574
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-04-18 09:54:06 -0400

    Pagination finally horizontal, beginnings of a functioning top bar. No patience for styling.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,28 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require foundation
-//= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+var page = 2;
+
+// stop default event from happening
+$('.more-sprouts').on('click', function(event) {
+  event.preventDefault();
+
+//existing list of tweets on html template
+  var tweets_list = $('.tweets');
+
+//ajax request to tweets api
+  var request = $.ajax({
+   method: "GET",
+   url: ("/tweets.json?page=" + page),
+  });
+
+//parsing the json and appending it to list
+  request.done(function(newTweets) {
+    for (var i = 0; i < newTweets.length; i++) {
+      tweetBody = newTweets[i]["text"];
+      tweetUser = newTweets[i]["username"];
+      tweets_list.append("<li class='tweet'>" + "<div class='body'>" + tweetBody + "</div>" + "<div class='user'>" + tweetUser + "</div>" + "</li>");
+    }
+    page++
+  });
+});
