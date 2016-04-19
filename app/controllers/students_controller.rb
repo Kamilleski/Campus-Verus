4da1ace8ebae0adf2cff2f59071d9c4694d81958
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-04-19 08:35:29 -0400

    sticky top bar and successful devise registration (fwiw)

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,32 +12,7 @@ class StudentsController < ApplicationController
     @names = School.names
   end
 
-  def create
-    @student = Student.new(student_params)
-    @names = School.names
-    if @student.save
-      flash[:notice] = "Account successfully created!"
-      redirect_to student_path(@student)
-    else
-      @college_years = Student::COLLEGE_YEARS
-      flash[:error] = @student.errors.full_messages.join(". ")
-      redirect_to new_student_registration_path
-    end
-  end
-
   private
-
-  def student_params
-    params.require(:student).permit(
-      :first_name,
-      :last_name,
-      :photo_url,
-      :interests,
-      :year_in_college,
-      :living_situation,
-      :hometown,
-      :description)
-  end
 
   def authorize_student
     if !student_signed_in? || !current_student.admin?
--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -5,9 +5,11 @@ class Tourist < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  SEASONS = ["Fall", "Spring", "Summer", "Winter"]
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :prospective_major, presence: true
-  validates :intended_start_date_semester, presence: true, inclusion: { in: ["Fall", "Spring",            "Summer", "Winter"] }
+  validates :intended_start_date_semester, presence: true, inclusion: { in: SEASONS }
   validates :intended_start_date_year, numericality: true
 end
