c835cf740d5e62c910e46a77147a7a9029715c5f
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-04-20 14:52:20 -0400

    All tests passing

--- spec/features/user_edits_account_information_spec.rb
+++ spec/features/user_edits_account_information_spec.rb
@@ -3,12 +3,73 @@ require 'rails_helper'
 feature 'user can edit their account as either a student or a tourist' do
   scenario 'student successfully edits profile' do
     freddie = FactoryGirl.create(:student)
-    login_as(freddie, :scope => :student)
+    visit new_student_session_path
 
+    fill_in "Email", with: freddie.email
+    fill_in "Password", with: freddie.password
+    click_button "Log in"
+
+    click_link "Your Account"
+
+    fill_in "student_photo_url", with: "http://pixel.nymag.com/imgs/daily/vulture/2015/12/03/03-freddie-mercury.w529.h529.jpg"
+    fill_in "student_current_password", with: freddie.password
+
+    click_button "Update"
+
+    expect(page).to have_content "Your account has been updated successfully."
   end
+
+  scenario 'student unsuccessfully edits profile' do
+    freddie = FactoryGirl.create(:student)
+    visit new_student_session_path
+
+    fill_in "Email", with: freddie.email
+    fill_in "Password", with: freddie.password
+    click_button "Log in"
+
+    click_link "Your Account"
+
+    fill_in "student_photo_url", with: "http://pixel.nymag.com/imgs/daily/vulture/2015/12/03/03-freddie-mercury.w529.h529.jpg"
+
+    click_button "Update"
+
+    expect(page).to have_content "Current password can't be blank"
+    expect(freddie.photo_url).to eq("https://pmcdeadline2.files.wordpress.com/2015/11/freddie-mercury.jpg")
+  end
+
   scenario 'tourist successfully edits profile' do
     hermione = FactoryGirl.create(:tourist)
-    login_as(hermione, :scope => :tourist)
+    visit new_tourist_session_path
 
+    fill_in "Email", with: hermione.email
+    fill_in "Password", with: hermione.password
+    click_button "Log in"
+
+    click_link "Your Account"
+
+    fill_in "tourist_prospective_major", with: "Classics"
+    fill_in "tourist_current_password", with: hermione.password
+
+    click_button "Update"
+
+    expect(page).to have_content "Your account has been updated successfully."
+  end
+
+  scenario 'tourist unsuccessfully edits profile' do
+    hermione = FactoryGirl.create(:tourist)
+    visit new_tourist_session_path
+
+    fill_in "Email", with: hermione.email
+    fill_in "Password", with: hermione.password
+    click_button "Log in"
+
+    click_link "Your Account"
+
+    fill_in "tourist_prospective_major", with: "Classics"
+
+    click_button "Update"
+
+    expect(page).to have_content "Current password can't be blank"
+    expect(hermione.prospective_major).to eq("Chemical Engineering")
   end
 end
--- spec/features/user_logs_in_and_out_spec.rb
+++ spec/features/user_logs_in_and_out_spec.rb
@@ -2,14 +2,37 @@ require 'rails_helper'
 
 feature 'user is able to sign in and out' do
   scenario 'user signs in as student' do
+    freddie = FactoryGirl.create(:student)
+    visit new_student_session_path
 
+    fill_in "Email", with: freddie.email
+    fill_in "Password", with: freddie.password
+    click_button "Log in"
+
+    expect(page).to_not have_content("error")
   end
 
   scenario 'user signs in as tourist' do
+    hermione = FactoryGirl.create(:tourist)
+    visit new_tourist_session_path
 
+    fill_in "Email", with: hermione.email
+    fill_in "Password", with: hermione.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully")
   end
 
   scenario 'user signs out' do
+    hermione = FactoryGirl.create(:tourist)
+    visit new_tourist_session_path
 
+    fill_in "Email", with: hermione.email
+    fill_in "Password", with: hermione.password
+    click_button "Log in"
+
+    click_link "Log Out"
+
+    expect(page).to have_content("Signed out successfully")
   end
 end
