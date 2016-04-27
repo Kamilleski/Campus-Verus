fbe75cfcddae024a1bc0715d45669afc11a9c468
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-04-27 14:21:54 -0700

    Devise Student Sign-up Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,8 +38,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     if resource_class == Student
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :photo_url, :interests, :year_in_college, :living_situation, :hometown, :password, :password_confirmation, :school_id) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :photo_url, :interests, :year_in_college, :living_situation, :hometown, :password, :password_confirmation, :current_password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :photo_url, :interests, :year_in_college, :major, :living_situation, :hometown, :password, :password_confirmation, :school_id) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :photo_url, :interests, :year_in_college, :major, :living_situation, :hometown, :password, :password_confirmation, :current_password) }
     end
 
     if resource_class == Tourist
